9f91bac6e25f7011061814e7f8cb688f9b5a8727
thiagovasconcelos <prof2131@iesp.edu.br>
2026-05-09 13:46:28 -0300

    Proketo Base

---
+++ src/test/java/serverrest/BaseTest.java
@@ -0,0 +1,46 @@
+package serverrest;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.BeforeAll;
+
+import static io.restassured.RestAssured.given;
+
+public class BaseTest {
+    public static String TOKEN;
+    public static String MSG_CADASTRO_USER_OK = "Cadastro realizado com sucesso";
+    public static String MSG_EMAIL_DUPLICADO = "Este email já está sendo usado";
+    public static String MSG_LOGIN_OK = "Login realizado com sucesso";
+    public static String MSG_LOGIN_NOK = "Email e/ou senha inválidos";
+
+
+
+    @BeforeAll
+    public static void setup() {
+        RestAssured.baseURI = "https://serverest.dev/";
+    }
+
+
+
+    ValidatableResponse doGet(String path, int statusCode){
+        return given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get(path)
+                .then()
+                .statusCode(statusCode);
+    }
+    static ValidatableResponse doPost(Object body, String path, int statusCode) {
+        return given()
+                .contentType(ContentType.JSON)
+                .body(body)
+                .when()
+                .post(path)
+                .then()
+                .statusCode(statusCode);
+    }
+
+
+}
